a5b34d7b2e019a9f509611d556318d781f3902e0
EGLipson <43010010+EGLipson@users.noreply.github.com>
2018-09-25 12:45:50 -0400

    Add files via upload

---
+++ Opperations_Practice.py
@@ -0,0 +1,22 @@
+'''
+Created on Sep 25, 2018
+
+@author: Emma Lipson
+'''
+
+
+print("Welcome to the test score calculator.")
+print("/n/n/n/n")
+
+#enter the test scores
+test1 = float(input("please enter your first test score: "))
+test2 = float(input("please enter your second test score: "))
+test3 = float(input("please enter your third test score: "))
+
+#calculate the average
+
+average = (test1 + test2 + test3) / 3
+
+#display the output
+
+print("Average test score:", average)
